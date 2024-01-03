1d685d0111dd1be92345d8d2416cf1df32849cb2
Piotr Mościcki / squares <m.piotr@moscicki.pl>
2024-01-03 16:10:28 +0000

    Add button changing visibility of the text on the screen

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
-let message: string = 'Hello, World!';
-console.log(message);
+const toggleButton = document.getElementById('toggleButton');
+const message = document.getElementById('message');
+
+// add checks for null
+if (toggleButton === null) {
+    throw new Error('toggleButton is null');
+}
+if (message === null) {
+    throw new Error('message is null');
+}
+
+toggleButton.addEventListener('click', () => {
+    message.style.display = message.style.display === 'none' ? 'block' : 'none';
+});
