adc2c14d7d281486bbddd54ed60c6f6086209c1d
Piotr Mościcki / squares <m.piotr@moscicki.pl>
2024-01-09 19:27:36 +0000

    Working node movement using drag and drop

--- src/main.ts
+++ src/main.ts
@@ -79,6 +79,20 @@ class Connection {
   to: number;
 }
 
+class IModelObserver {
+  onModelChanged(): void {}
+
+  onNodeCreated(_index: number): void {}
+  onNodeDestroyed(_index: number): void {}
+  onNodeNameChanged(_index: number): void {}
+  onNodeRectangleChanged(_index: number): void {}
+  onNodeChildAdded(_parent: number, _child: number): void {}
+  onNodeChildRemoved(_parent: number, _child: number): void {}
+  onConnectionAdded(_from: number, _to: number): void {}
+  onConnectionRemoved(_from: number, _to: number): void {}
+}
+
+
 class Model {
   // Accessors
   getRoot(): number {
@@ -106,6 +120,14 @@ class Model {
   }
 
   // Mutators
+  registerObserver(observer: IModelObserver): void {
+    this.observers.push(observer);
+  }
+
+  unregisterObserver(observer: IModelObserver): void {
+    this.observers = this.observers.filter(item => item !== observer);
+  }
+
   createNode(index: number = -1): number {
     if (this.nodes.indexOf(index) !== -1 || index >= this.indexGenerator) {
       throw new Error('Index is already used or is greater than the index generator');
@@ -114,6 +136,8 @@ class Model {
       index = this.indexGenerator++;
     }
     this.nodes.push(index);
+    this.observers.forEach(observer => observer.onNodeCreated(index));
+    this.observers.forEach(observer => observer.onModelChanged());
     return index;
   }
 
@@ -123,20 +147,28 @@ class Model {
     this.rectangles.delete(index);
     this.children.delete(index);
     this.connections = this.connections.filter(connection => connection.from !== index && connection.to !== index);
+    this.observers.forEach(observer => observer.onNodeDestroyed(index));
+    this.observers.forEach(observer => observer.onModelChanged());
   }
 
   setName(index: number, name: string): void {
     this.names.set(index, name);
+    this.observers.forEach(observer => observer.onNodeNameChanged(index));
+    this.observers.forEach(observer => observer.onModelChanged());
   }
 
   setRectangle(index: number, rectangle: Rectangle): void {
     this.rectangles.set(index, rectangle);
+    this.observers.forEach(observer => observer.onNodeRectangleChanged(index));
+    this.observers.forEach(observer => observer.onModelChanged());
   }
 
   addChild(parent: number, child: number): void {
     const children = this.children.get(parent) || [];
     children.push(child);
     this.children.set(parent, children);
+    this.observers.forEach(observer => observer.onNodeChildAdded(parent, child));
+    this.observers.forEach(observer => observer.onModelChanged());
   }
 
   removeChild(parent: number, child: number): void {
@@ -145,17 +177,24 @@ class Model {
     if (index !== -1) {
       children.splice(index, 1);
     }
+    this.children.set(parent, children);
+    this.observers.forEach(observer => observer.onNodeChildRemoved(parent, child));
   }
 
   addConnection(from: number, to: number): void {
     this.connections.push(new Connection(from, to));
+    this.observers.forEach(observer => observer.onConnectionAdded(from, to));
+    this.observers.forEach(observer => observer.onModelChanged());
   }
 
   removeConnection(from: number, to: number): void {
     this.connections = this.connections.filter(connection => connection.from !== from || connection.to !== to);
+    this.observers.forEach(observer => observer.onConnectionRemoved(from, to));
+    this.observers.forEach(observer => observer.onModelChanged());
   }
 
   // Private members
+  private observers: IModelObserver[] = [];
   private indexGenerator: number = 1;
   private nodes: number[] = [];
   private names: Map<number, string> = new Map<number, string>();
@@ -188,14 +227,84 @@ class ViewModel {
   private displayedParent: number = 0;
 }
 
-class View {
+class IViewController {
+  isActive(): boolean { return false; }
+  onMouseDown(_event: MouseEvent): void {}
+  onMouseMove(_event: MouseEvent): void {}
+  onMouseUp(_event: MouseEvent): void {}
+}
+
+class NodeMoveController {
+  constructor(viewModel: ViewModel) {
+    this.viewModel = viewModel;
+  }
+
+  // IViewController
+  isActive(): boolean { return this.active; }
+
+  onMouseDown(_event: MouseEvent): void {}
+
+  onMouseMove(event: MouseEvent): void {
+    // if controller is not active, mouse is moving and LMB is pressed then set controller active
+    if (!this.active && event.buttons === 1) {
+      // if mouse is hovered over a node then move it and set controller active
+      const displayedParent = this.viewModel.getDisplayedParent();
+      const children = this.viewModel.getModel().getChildren(displayedParent);
+      const rectangles = children.map(child => this.viewModel.getModel().getRectangle(child));
+      const index = rectangles.findIndex(rectangle => event.clientX >= rectangle.x 
+          && event.clientX <= rectangle.x + rectangle.width 
+          && event.clientY >= rectangle.y 
+          && event.clientY <= rectangle.y + rectangle.height);
+      if (index !== -1) {
+        this.active = true;
+        this.draggedNode = children[index];
+        this.startingCursorPosition = { x: event.clientX, y: event.clientY };
+        this.startingRectangle = this.viewModel.getModel().getRectangle(this.draggedNode);
+      }
+    }
+    
+    if (this.active) {
+      // move node
+      const deltaX = event.clientX - this.startingCursorPosition.x;
+      const deltaY = event.clientY - this.startingCursorPosition.y;
+      const rectangle = new Rectangle(this.startingRectangle.x + deltaX, this.startingRectangle.y + deltaY, 
+        this.startingRectangle.width, this.startingRectangle.height);
+      this.viewModel.getModel().setRectangle(this.draggedNode, rectangle);
+    }
+  }
+
+  onMouseUp(_event: MouseEvent): void {
+    this.active = false;
+    this.draggedNode = -1;
+    this.startingCursorPosition = { x: 0, y: 0 };
+    this.startingRectangle = new Rectangle(0, 0, 0, 0);
+  }
+
+
+  // Private members
+  private viewModel: ViewModel;
+  private active: boolean = false;
+  private draggedNode: number = -1;
+  private startingCursorPosition: { x: number, y: number } = { x: 0, y: 0 };
+  private startingRectangle: Rectangle = new Rectangle(0, 0, 0, 0);
+}
+
+class View implements IModelObserver {
   constructor(viewModel: ViewModel, canvas: HTMLCanvasElement) {
     this.viewModel = viewModel;
     this.canvas = canvas;
     this.ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
-    // log size
-    console.log('canvas width: ' + canvas.width);
-    console.log('canvas height: ' + canvas.height);
+    
+    // register as observer
+    this.viewModel.getModel().registerObserver(this as IModelObserver);
+
+    // create controllers
+    this.controllers.push(new NodeMoveController(this.viewModel));
+
+    // add event listeners
+    canvas.addEventListener('mousedown', (event) => this.onEvent(controller => controller.onMouseDown(event)));
+    canvas.addEventListener('mousemove', (event) => this.onEvent(controller => controller.onMouseMove(event)));
+    canvas.addEventListener('mouseup', (event) => this.onEvent(controller => controller.onMouseUp(event)));
   }
 
   draw(): void {
@@ -281,10 +390,55 @@ class View {
     }
   }
 
+  // Generic function for controlling active controller. Takes lambda as a parameter and calls it 
+  // on controllers until one of them becomes active. Then it stops calling the lambda and sets the
+  // active controller.
+  // This function is used by event handlers.
+  onEvent(lambda: (controller: IViewController) => void): void {
+    // if there is an active controller then pass the event to it
+    if (this.activeController !== null) {
+      lambda(this.activeController);
+      if (!this.activeController.isActive()) {
+        this.activeController = null;
+      }
+      return;
+    }
+    // if there is no active controller then pass the event to all controllers until one of them 
+    // becomes active - then we stop passing the event to other controllers
+    for (let controller of this.controllers) {
+      lambda(controller);
+      if (controller.isActive()) {
+        this.activeController = controller;
+        break;
+      }
+    }
+  }
+
+  // IModelObserver
+  onModelChanged(): void {
+    this.draw();
+  }
+
+  onNodeCreated(_index: number): void {}
+  onNodeDestroyed(_index: number): void {}
+  onNodeNameChanged(_index: number): void {}
+  onNodeRectangleChanged(_index: number): void {}
+  onNodeChildAdded(_parent: number, _child: number): void {}
+  onNodeChildRemoved(_parent: number, _child: number): void {}
+  onConnectionAdded(_from: number, _to: number): void {}
+  onConnectionRemoved(_from: number, _to: number): void {}
+  // end IModelObserver
+
   // Private members
   private viewModel: ViewModel;
   private canvas: HTMLCanvasElement;
   private ctx: CanvasRenderingContext2D;
+
+  // if there is an active controller it will receive all events
+  // if there is no active controller then all events will be passed to all controllers until one 
+  // of them becomes active
+  private controllers: IViewController[] = [];
+  private activeController: IViewController | null = null;
 }
 
 var model = new Model();
