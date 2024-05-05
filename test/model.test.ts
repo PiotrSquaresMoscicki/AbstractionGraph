cdc3da9af8e09e943979e45da96fc8b937347c79
Piotr Squares Mościcki <piotr.squares.moscicki@gmail.com>
2024-05-05 19:02:22 +0000

    Add jest testing framework

---
+++ test/model.test.ts
@@ -0,0 +1,14 @@
+import { Model } from '../src/model';
+
+describe('Model', () => {
+  let model: Model;
+
+  beforeEach(() => {
+    model = new Model();
+  });
+
+  test('Root node equals to 0', () => {
+    const root = model.getRoot();
+    expect(root).toEqual(0);
+  });
+});
