eede24cb0b8817eb2934448cc9806402d8b3f6f7
Piotr Squares Mościcki <piotr.squares.moscicki@gmail.com>
2024-05-06 05:51:24 +0000

    Add more automated tests for model

--- src/test/model.test.ts
+++ src/test/model.test.ts
@@ -111,3 +111,131 @@ describe('Getting driveshaft rectangle in root reference frame should throw an e
 });
 
 //************************************************************************************************
+describe('Getting a driveshaft rectangle in a non-existing reference frame should throw an error', () => {
+  let model: Model;
+
+  beforeEach(() => {
+    model = new Model();
+    createSampleGraph(model);
+  });
+
+  test('Getting a driveshaft rectangle in a non-existing reference frame should throw an error', () => {
+    const driveshaftNodes= model.getNodesWithName('Driveshaft');
+    expect(driveshaftNodes.length).toEqual(1);
+    const driveshaftNode = driveshaftNodes[0];
+    expect(() => model.getRectangle(driveshaftNode, 123)).toThrow();
+  });
+});
+
+//************************************************************************************************
+describe('Getting a non-existing node rectangle should throw an error', () => {
+  let model: Model;
+
+  beforeEach(() => {
+    model = new Model();
+    createSampleGraph(model);
+  });
+
+  test('Getting a non-existing node rectangle should throw an error', () => {
+    expect(() => model.getRectangle(123, model.getRoot())).toThrow();
+  });
+});
+
+//************************************************************************************************
+describe('Get driveshaft name', () => {
+  let model: Model;
+
+  beforeEach(() => {
+    model = new Model();
+    createSampleGraph(model);
+  });
+
+  test('Get driveshaft name', () => {
+    const driveshaftNodes= model.getNodesWithName('Driveshaft');
+    expect(driveshaftNodes.length).toEqual(1);
+    const driveshaftNode = driveshaftNodes[0];
+    expect(model.getName(driveshaftNode)).toEqual('Driveshaft');
+  });
+});
+
+//************************************************************************************************
+describe('Non existing node name should be equal to an empty string', () => {
+  let model: Model;
+
+  beforeEach(() => {
+    model = new Model();
+    createSampleGraph(model);
+  });
+
+  test('Non existing node name should be equal to an empty string', () => {
+    expect(model.getName(123)).toEqual('');
+  });
+});
+
+//************************************************************************************************
+describe('Get engine children', () => {
+  let model: Model;
+
+  beforeEach(() => {
+    model = new Model();
+    createSampleGraph(model);
+  });
+
+  test('Get engine children', () => {
+    const engineNodes= model.getNodesWithName('Engine');
+    expect(engineNodes.length).toEqual(1);
+    const engineNode = engineNodes[0];
+    const children = model.getChildren(engineNode);
+    expect(children.length).toEqual(2);
+    expect(model.getName(children[0])).toEqual('Pistons');
+    expect(model.getName(children[1])).toEqual('Crankshaft');
+  });
+});
+
+//************************************************************************************************
+describe('Get children of a non-existing node should return an empty array', () => {
+  let model: Model;
+
+  beforeEach(() => {
+    model = new Model();
+    createSampleGraph(model);
+  });
+
+  test('Get children of a non-existing node should return an empty array', () => {
+    const children = model.getChildren(123);
+    expect(children.length).toEqual(0);
+  });
+});
+
+//************************************************************************************************
+describe('Get engine parent', () => {
+  let model: Model;
+
+  beforeEach(() => {
+    model = new Model();
+    createSampleGraph(model);
+  });
+
+  test('Get engine parent', () => {
+    const engineNodes= model.getNodesWithName('Engine');
+    expect(engineNodes.length).toEqual(1);
+    const engineNode = engineNodes[0];
+    const parent = model.getParent(engineNode) as number;
+    expect(model.getName(parent)).toEqual('Car');
+  });
+});
+
+//************************************************************************************************
+describe('Get parent of a non-existing node should return null', () => {
+  let model: Model;
+
+  beforeEach(() => {
+    model = new Model();
+    createSampleGraph(model);
+  });
+
+  test('Get parent of a non-existing node should return null', () => {
+    const parent = model.getParent(123);
+    expect(parent).toBeNull();
+  });
+});
